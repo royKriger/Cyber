9d1d3ca7b70a88ed86a1c47ef2c1e7a46cbc60eb
royKriger <roy.kriger@gmail.com>
2025-04-08 17:24:05 +0300

    Delete UDP directory

--- UDP/client.py
+++
@@ -1,6 +0,0 @@
-import socket
-
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.sendto(f"Omer".encode(), ("127.0.0.1", 8200))
-data, temp = my_socket.recvfrom(1024)
-print(data.decode())
